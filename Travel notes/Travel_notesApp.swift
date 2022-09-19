7bd21573124f56c695722d747eeea03efd1acac4
Hayato Watanabe <solideye@me.com>
2022-09-19 09:16:36 +0900

    Initial Commit

---
+++ Travel notes/Travel_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_notesApp.swift
+//  Travel notes
+//
+//  Created by Hayato Watanabe on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct Travel_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
